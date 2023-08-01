fd03e61440255bcca5be8d2388fc9195b5d168a2
Silleellie <antonio.silletti@gmail.com>
2023-08-01 23:52:06 +0200

    added init files

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+import os
+from pathlib import Path
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_PATH = str(Path(os.path.join(THIS_DIR, "..")).resolve())
+
+DATA_DIR = os.path.join(ROOT_PATH, "data")
+SPLITS_DIR = os.path.join(DATA_DIR, "splits")
+
+RANDOM_STATE = 42
