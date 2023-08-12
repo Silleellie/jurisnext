4056d54f7997fce80581e9d5a72019d062356051
Silleellie <antonio.silletti@gmail.com>
2023-08-12 17:36:03 +0200

    init models dir

--- src/__init__.py
+++ src/__init__.py
@@ -6,5 +6,6 @@ ROOT_PATH = str(Path(os.path.join(THIS_DIR, "..")).resolve())
 
 DATA_DIR = os.path.join(ROOT_PATH, "data")
 SPLITS_DIR = os.path.join(DATA_DIR, "splits")
+MODELS_DIR = os.path.join(ROOT_PATH, "models")
 
 RANDOM_STATE = 42
