52bb28ec6411b25b8e4b8f127b7f0283bcfbbcdb
Silleellie <antonio.silletti@gmail.com>
2023-08-16 13:13:29 +0200

    WIP: added fusion strategies for bert sentence encoding
    Co-authored-by: m-elio <m-elio@users.noreply.github.com>

--- src/sentence_encoders.py
+++ src/sentence_encoders.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import Tuple, Literal
 
 import torch
 import numpy as np
@@ -10,49 +11,71 @@ from transformers import BertModel, BertTokenizerFast
 class SentenceEncoder(ABC):
 
     @abstractmethod
-    def get_sentence_embeddings(self, sentences):
+    def __call__(self, *sentences: Tuple[str]) -> np.ndarray:
         raise NotImplementedError
 
 
-class SbertSentenceEncoder(SentenceEncoder):
+class SentenceTransformerEncoder(SentenceEncoder):
 
     def __init__(self, model_name='all-MiniLM-L6-v2', model_kwargs=None):
 
-        super().__init__()
-
         if model_kwargs is None:
-
             model_kwargs = {}
 
         self.model = SentenceTransformer(model_name, **model_kwargs)
 
-    def get_sentence_embeddings(self, sentences):
-        return self.model.encode(sentences)
+    def __call__(self, *sentences: Tuple[str]):
+        return self.model.encode(*sentences)
 
 
 class BertSentenceEncoder(SentenceEncoder):
 
-    def __init__(self, model_name='bert-base-uncased', hidden_states_num=4, model_kwargs=None):
-
-        super().__init__()
+    def __init__(self, model_name='bert-base-uncased',
+                 hidden_states_num=4,
+                 hidden_states_fusion_strat: Literal["sum", "concat"] = "sum",
+                 token_fusion_strat: Literal["sum", "mean"] = "sum",
+                 model_kwargs=None):
 
         if model_kwargs is None:
-
             model_kwargs = {}
+
+        # keepdim to avoid implicit squeeze when one dim becomes 1
+        sum_fusion = lambda x, y: torch.sum(x, dim=y, keepdim=True)
+        concat_fusion = lambda x, y: torch.cat(x, dim=y)
+        mean_fusion = lambda x, y: torch.mean(x, dim=y, keepdim=True)
+
+        # fusing hidden states involves manipulating dim 0 of the stacked tensor
+        hidden_states_available_fusions = {"sum": lambda x: sum_fusion(x, 0), "concat": lambda x: concat_fusion(x, 0)}
+
+        # fusing token embeddings to get a single sentence embedding involves manipluating dim 1 of the stacked tensor
+        token_available_fusions = {"sum": lambda x: sum_fusion(x, 1), "mean": lambda x: mean_fusion(x, 1)}
+
+
+        self.hidden_states_fusion_strat = hidden_states_available_fusions[hidden_states_fusion_strat]
+        self.token_fusion_strat = token_available_fusions[token_fusion_strat]
 
         self.tokenizer = BertTokenizerFast.from_pretrained(model_name)
         self.model = BertModel.from_pretrained(model_name, output_hidden_states=True, **model_kwargs)
-        self.hidden_states_num = - hidden_states_num
+        self.hidden_states_num = hidden_states_num
 
-    def get_sentence_embeddings(self, sentences):
+    def __call__(self, *sentences: Tuple[str]):
 
         outputs = []
 
         for sentence in sentences:
 
             with torch.no_grad():
-                output_hidden_states = self.model(**self.tokenizer(sentence, return_tensors='pt')).hidden_states[self.hidden_states_num:]
-                output = torch.vstack(output_hidden_states).squeeze().sum(dim=[0, 1]).numpy()
-                outputs.append(output)
+                tokenized_sentences = self.tokenizer(sentence, return_tensors='pt', truncation=True)
+                output_hidden_states = self.model(**tokenized_sentences).hidden_states[-self.hidden_states_num:]
+                output_stacked = torch.vstack(output_hidden_states)
+
+                # TO DO: keep track of dimensions during fusion (at the end dim (1 x hidden_states_optionally_concat)
+                sentence_embedding = self.token_fusion_strat(output_stacked)
+
+                hidden_states_fused = self.hidden_states_fusion_strat(sentence_embedding)
+
+                # sentence_embedding = sentence_embedding.squeeze(dim=0)
+
+                outputs.append(sentence_embedding)
 
         return np.vstack(outputs)
