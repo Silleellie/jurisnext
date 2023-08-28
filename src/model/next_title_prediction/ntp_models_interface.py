2afa578524a92e0850bb7b5fc782a2fae4a43b08
Silleellie <antonio.silletti@gmail.com>
2023-08-29 01:33:11 +0200

    major overhaul abstraction ntp models
    Co-authored-by: m-elio <m-elio@users.noreply.github.com>

--- src/model/next_title_prediction/ntp_models_interface.py
+++ src/model/next_title_prediction/ntp_models_interface.py
@@ -1,48 +1,39 @@
 import os
 import pickle
-from abc import abstractmethod
+from abc import abstractmethod, ABC
 from typing import Union
 
 import numpy as np
-import torch
-import transformers.optimization
-from transformers import PreTrainedModel, PreTrainedTokenizer, PreTrainedTokenizerFast
+from transformers import PreTrainedModel, PreTrainedTokenizer, PreTrainedTokenizerFast, AutoTokenizer
 
 from src.model.clustering import ClusterLabelMapper
 
 
+class NTPConfig:
+
+    def __init__(self, device: str = "cpu"):
+        self.device = device
+
+
 # interface for all sequence classification models
-class NextTitlePredictor:
+class NTPModel:
 
     model_class = None
 
     def __init__(self,
                  model: PreTrainedModel,
                  tokenizer: Union[PreTrainedTokenizer, PreTrainedTokenizerFast],
-                 optimizer: Union[torch.optim.Optimizer, transformers.optimization.Optimizer],
-                 cluster_label_mapper: ClusterLabelMapper = None,
-                 device: str = "cuda:0"):
+                 cluster_label_mapper: ClusterLabelMapper = None):
 
         self.model = model
         self.tokenizer = tokenizer
-        self.optimizer = optimizer
         self.cluster_label_mapper = cluster_label_mapper
-        self.device = device
-        self.model.to(device)
 
-    @property
-    def config(self):
-        return self.model.config
+        self.model.to(self.model.config.device)
 
-    def eval(self):
-        return self.model.eval()
-
-    def train(self):
-        return self.model.train()
-
-    @property
-    def training(self):
-        return self.model.training
+    @abstractmethod
+    def get_suggested_optimizer(self):
+        raise NotImplementedError
 
     @abstractmethod
     def tokenize(self, sample):
@@ -60,38 +51,90 @@ class NextTitlePredictor:
     def valid_step(self, batch):
         raise NotImplementedError
 
+    @property
+    def config(self):
+        return self.model.config
+
+    def train(self, mode: bool = True):
+        return self.model.train(mode)
+
+    def eval(self):
+        return self.model.eval()
+
+    @property
+    def training(self):
+        return self.model.training
+
     def _train_clusters(self, unique_train_labels: np.ndarray, all_labels: np.ndarray):
         # fit the cluster label mapper with train labels and all labels which should be clustered (both are unique)
         self.cluster_label_mapper.fit(unique_train_labels, all_labels)
 
-    def save_pretrained(self, save_path):
+    def save(self, save_path):
 
         self.model.save_pretrained(save_path)
+        self.tokenizer.save_pretrained(save_path)
 
-        with open(os.path.join(save_path, 'cluster_label_mapper.pkl'), "wb") as f:
-            pickle.dump(self.cluster_label_mapper, f)
-
-        torch.save(self.optimizer.state_dict(), os.path.join(save_path, 'optimizer.pt'))
+        if self.cluster_label_mapper is not None:
+            with open(os.path.join(save_path, 'cluster_label_mapper.pkl'), "wb") as f:
+                pickle.dump(self.cluster_label_mapper, f)
 
     @classmethod
-    def from_pretrained(cls, save_path, **kwargs):
+    def load(cls, save_path):
 
         model = cls.model_class.from_pretrained(
             pretrained_model_name_or_path=save_path
         )
 
-        with open(os.path.join(save_path, 'cluster_label_mapper.pkl'), "rb") as f:
-            cluster_label_mapper = pickle.load(f)
+        tokenizer = AutoTokenizer.from_pretrained(
+            pretrained_model_name_or_path=save_path
+        )
+
+        cluster_label_mapper_path = os.path.join(save_path, 'cluster_label_mapper.pkl')
+
+        cluster_label_mapper = None
+        if os.path.isfile(cluster_label_mapper_path):
+            with open(cluster_label_mapper_path, "rb") as f:
+                cluster_label_mapper = pickle.load(f)
 
         new_inst = cls(
             model=model,
-            cluster_label_mapper=cluster_label_mapper,
-            **kwargs
+            tokenizer=tokenizer,
+            cluster_label_mapper=cluster_label_mapper
         )
-
-        new_inst.optimizer.load_state_dict(torch.load(os.path.join(save_path, 'optimizer.pt')))
 
         return new_inst
 
     def __call__(self, *args, **kwargs):
         return self.model(*args, **kwargs)
+
+
+class NTPModelHF(ABC, NTPModel):
+
+    config_class = NTPConfig
+
+    def __init__(self,
+                 pretrained_model_or_pth: str,
+                 cluster_label_mapper: ClusterLabelMapper = None,
+                 **config_kwargs):
+
+        self.model_class.config_class = self.config_class
+        model = self.model_class.from_pretrained(pretrained_model_or_pth, **config_kwargs)
+        tokenizer = AutoTokenizer.from_pretrained(pretrained_model_or_pth)
+
+        super().__init__(model, tokenizer, cluster_label_mapper)
+
+    @classmethod
+    def load(cls, save_path):
+        cls.model_class.config_class = cls.config_class
+
+        cluster_label_mapper_path = os.path.join(save_path, 'cluster_label_mapper.pkl')
+
+        cluster_label_mapper = None
+        if os.path.isfile(cluster_label_mapper_path):
+            with open(cluster_label_mapper_path, "rb") as f:
+                cluster_label_mapper = pickle.load(f)
+
+        return cls(
+            pretrained_model_or_pth=save_path,
+            cluster_label_mapper=cluster_label_mapper
+        )
