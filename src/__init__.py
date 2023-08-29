996d3a4c8c1e8ead93b6e06ed1ce946bc06b3bee
Silleellie <antonio.silletti@gmail.com>
2023-08-29 13:19:52 +0200

    created ExperimentConfig dataclass
    Co-authored-by: m-elio <m-elio@users.noreply.github.com>

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,10 @@
 import os
+from dataclasses import dataclass
 from pathlib import Path
+from typing import Optional
 
-THIS_DIR = os.path.dirname(os.path.abspath(__file__))
-ROOT_PATH = str(Path(os.path.join(THIS_DIR, "..")).resolve())
+_THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_PATH = str(Path(os.path.join(_THIS_DIR, "..")).resolve())
 
 DATA_DIR = os.path.join(ROOT_PATH, "data")
 RAW_DATA_DIR = os.path.join(DATA_DIR, "raw")
@@ -10,4 +12,13 @@ PROCESSED_DATA_DIR = os.path.join(DATA_DIR, "processed")
 INTERIM_DATA_DIR = os.path.join(DATA_DIR, "interim")
 MODELS_DIR = os.path.join(ROOT_PATH, "models")
 
-RANDOM_STATE = 42
+
+@dataclass
+class ExperimentConfig:
+    epochs: int = 100
+    batch_size: int = 2
+    eval_batch_size: int = 2
+    use_cluster_alg: bool = True
+    checkpoint: Optional[str] = None
+    device: str = "cuda:0"
+    random_state: int = 42
