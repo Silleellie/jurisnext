13b461f9f20f3cb26ad6281c4714de5ff08f0e02
Silleellie <antonio.silletti@gmail.com>
2023-08-31 17:37:25 +0200

    completed metrics abstraction for eval
    Co-authored-by: m-elio <m-elio@users.noreply.github.com>

--- src/evaluation/metrics.py
+++ src/evaluation/metrics.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
 
-from multimethod import multimethod
+import torch
+import torchmetrics.functional as torchmetrics_fn
 import numpy as np
+from sklearn.preprocessing import LabelEncoder
 
 
 class Metric(ABC):
@@ -11,60 +13,164 @@ class Metric(ABC):
     def __call__(self, predictions: np.ndarray[np.ndarray[str] | str], truths: np.ndarray[str]) -> float:
         raise NotImplementedError
 
+    @abstractmethod
+    def __str__(self):
+        raise NotImplementedError
 
-class Hit(Metric):
+
+class ClassificationMetric(Metric):
+
+    @abstractmethod
+    def compute_metric(self, predictions: torch.IntTensor, truths: torch.IntTensor, n_labels: int):
+        raise NotImplementedError
+
+    def __call__(self, predictions: np.ndarray[str], truths: np.ndarray[str]) -> float:
+        total = np.hstack((predictions, truths))
+
+        le = LabelEncoder().fit(total)
+        predictions = torch.from_numpy(le.transform(predictions)).int()
+        truths = torch.from_numpy(le.transform(truths)).int()
+
+        num_classes = len(np.unique(total))
+
+        res = self.compute_metric(predictions, truths, num_classes)
+
+        # k is not used, there's no list to cut
+        return res.item()
+
+    def __str__(self):
+
+        return f"{self.__class__.__name__} (weighted)"
+
+
+class Accuracy(ClassificationMetric):
+
+    def compute_metric(self, predictions: torch.IntTensor, truths: torch.IntTensor, n_labels: int):
+        return torchmetrics_fn.classification.multiclass_accuracy(predictions,
+                                                                  truths,
+                                                                  num_classes=n_labels,
+                                                                  average="weighted")
+
+
+class Precision(ClassificationMetric):
+
+    def compute_metric(self, predictions: torch.IntTensor, truths: torch.IntTensor, n_labels: int):
+        return torchmetrics_fn.classification.multiclass_precision(predictions,
+                                                                   truths,
+                                                                   num_classes=n_labels,
+                                                                   average="weighted")
+
+
+class Recall(ClassificationMetric):
+
+    def compute_metric(self, predictions: torch.IntTensor, truths: torch.IntTensor, n_labels: int):
+        return torchmetrics_fn.classification.multiclass_recall(predictions,
+                                                                truths,
+                                                                num_classes=n_labels,
+                                                                average="weighted")
+
+
+class F1(ClassificationMetric):
+
+    def compute_metric(self, predictions: torch.IntTensor, truths: torch.IntTensor, n_labels: int):
+        return torchmetrics_fn.classification.multiclass_f1_score(predictions,
+                                                                  truths,
+                                                                  num_classes=n_labels,
+                                                                  average="weighted")
+
+
+class RankingMetric(Metric):
 
     def __init__(self, k: int = None):
         self.k = k
 
-    @multimethod
-    def __call__(self, predictions: np.ndarray[str], truths: np.ndarray[str]) -> float:
-
-        # k is not used, there's no list to cut
-        return np.mean(predictions == truths).item()
-
-    @multimethod
-    def __call__(self, predictions: np.ndarray[np.ndarray[str]], truths: np.ndarray[str]) -> float:
-
-        predictions = np.array(predictions)
-        truths = np.array(truths)
-
-        predictions = predictions[:, :self.k] if self.k is not None else predictions
-
-        return np.mean(np.isin(truths, predictions)).item()
-
     def __str__(self):
-
-        string = "Hit"
+        string = self.__class__.__name__
         if self.k is not None:
             string += f"@{self.k}"
+
+        string += " (weighted)"
 
         return string
 
 
-if __name__ == "__main__":
+class Hit(RankingMetric):
 
-    metric = Hit()
-    metric_cut = Hit(k=1)
+    def __call__(self, predictions: np.ndarray[np.ndarray[str]], truths: np.ndarray[str]) -> float:
+        predictions = predictions[:, :self.k] if self.k is not None else predictions
 
-    predictions_single = ["a", "b", "c", "d", "e"]
-    predictions_multiple = [
-        np.array(["f", "b", "e"]),
-        np.array(["d", "e", "c"]),
-        np.array(["c", "c", "f"]),
-        np.array(["f", "f", "a"]),
-        np.array(["e", "a", "b"])
-    ]
-    truth = ["a", "g", "c", "x", "e"]
+        return np.mean(np.isin(truths, predictions)).item()
 
-    predictions_single = np.array(predictions_single)
-    predictions_multiple = np.array(predictions_multiple)
-    truth = np.array(truth)
 
-    print("OVERLOAD BABY 🔥")
-    print(metric(predictions_single, truth))
-    print(metric(predictions_multiple, truth))
+class MAP(RankingMetric):
 
-    print("OVERLOAD BABY CUT 🔥✂️")
-    print(metric_cut(predictions_single, truth))
-    print(metric_cut(predictions_multiple, truth))
+    def __call__(self, predictions: np.ndarray[np.ndarray[str]], truths: np.ndarray[str]) -> float:
+
+        predictions = predictions[:, :self.k] if self.k is not None else predictions
+
+        aps = []
+
+        for pred, truth in zip(predictions, truths):
+
+            pred: np.ndarray
+            truth: np.ndarray
+
+            # all items both in prediction and relevant truth are retrieved, if an item only appears in prediction it is
+            # marked with a -1, we then retrieve only the indexes of items that appear in both
+            hits: np.ndarray = (pred == truth).nonzero()[0]
+
+            if len(hits) == 0:
+                return 0
+
+            # we initialize an array for true positives. True positive is incremented by 1 each time a relevant item
+            # is found, therefore this array will be as long as the array containing the indices of items both in
+            # prediction and relevant truth (and values will be as such [1, 2, 3, ...])
+            tp_array = np.arange(start=1, stop=len(hits) + 1)
+
+            # finally, precision is computed by dividing each true positive value to each corresponding position
+            precision_array = tp_array / (hits + 1)
+            cumulative_precision = np.sum(precision_array)
+
+            # in this case we only have one item in truth
+            truth_ap = (1 / len(hits)) * cumulative_precision
+
+            aps.append(truth_ap)
+
+        map = np.mean(aps).item()
+
+        return map
+
+    def __str__(self):
+        return "MAP"
+
+
+class MRR(RankingMetric):
+
+    def __call__(self, predictions: np.ndarray[np.ndarray[str]], truths: np.ndarray[str]) -> float:
+
+        predictions = predictions[:, :self.k] if self.k is not None else predictions
+
+        rrs = []
+
+        for pred, truth in zip(predictions, truths):
+
+            pred: np.ndarray
+            truth: np.ndarray
+
+            rel_rank: np.ndarray = pred == truth
+            non_zero_rel_rank = np.argwhere(rel_rank).flatten()
+
+            if len(non_zero_rel_rank) > 0:
+                # + 1 because the array starts from 0
+                first_rel_rank = non_zero_rel_rank[0] + 1
+                rr = 1 / first_rel_rank
+                rrs.append(rr)
+            else:
+                rr = 0
+                rrs.append(rr)
+
+        mrrs = np.mean(rrs).item()
+        return mrrs
+
+    def __str__(self):
+        return "MRR"
