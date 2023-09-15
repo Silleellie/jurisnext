8db5b10914cca386658a2c5398aed7e00b6acf0d
Silleellie <antonio.silletti@gmail.com>
2023-09-15 13:41:34 +0200

    fix empty wandb update config dict

--- pipeline.py
+++ pipeline.py
@@ -179,8 +179,6 @@ if __name__ == '__main__':
                     wandb_dict["t5_tasks"] = exp_config.t5_tasks
                     wandb_dict["t5_keyword_min_occ"] = exp_config.t5_keyword_min_occ
 
-                wandb.config.update()
-
             model_name = model_train_func(exp_config)  # each main will use ExperimentConfig instance parameters
             model_path = os.path.join(MODELS_DIR, exp_config.exp_name)
 
